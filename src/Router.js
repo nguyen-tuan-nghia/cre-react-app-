f63b6a687546760113cce0c1864a1d6deb1c3266
nghia <tuannghia1406@gmail.com>
2022-08-29 15:45:32 +0700

    make it better

--- src/Router.js
+++ src/Router.js
@@ -1,47 +1,21 @@
-import React, { Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-const Layout = React.lazy(() => import("./pages/Layout"));
-const Home = React.lazy(() => import("./pages/Home/Home"));
-const Contact = React.lazy(() => import("./pages/Home/Contact"));
-const NoPage = React.lazy(() => import("./pages/Home/NoPage"));
+import loadable from "@loadable/component";
+const Layout = loadable(() => import("./pages/Layout"));
+const Home = loadable(() => import("./pages/Home/Home"));
+const Contact = loadable(() => import("./pages/Home/Contact"));
+const NotFoundPage = loadable(() => import("./pages/Home/NoPage"), {
+  modules: ["NotFoundPage"],
+});
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <Suspense fallback={<div>Loading...</div>}>
-              <Layout />
-            </Suspense>
-          }
-        >
-          <Route
-            index
-            element={
-              <Suspense fallback={<div>Loading...</div>}>
-                <Home />
-              </Suspense>
-            }
-          />
-          <Route
-            path="contact"
-            element={
-              <Suspense fallback={<div>Loading...</div>}>
-                <Contact />
-              </Suspense>
-            }
-          />
-          <Route
-            path="*"
-            element={
-              <Suspense fallback={<div>Loading...</div>}>
-                <NoPage />
-              </Suspense>
-            }
-          />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
--- src/index.js
+++ src/index.js
@@ -1,15 +1,54 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot, hydrateRoot } from "react-dom/client";
 import Router from "./Router";
 import reportWebVitals from "./reportWebVitals";
 import { HelmetProvider } from "react-helmet-async";
-import "bootstrap/dist/css/bootstrap.css";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <Router />
-    </HelmetProvider>
-  </React.StrictMode>
-);
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import { loadComponents } from "loadable-components";
+window.snapSaveState = () =>{
+  document.querySelector("#root").setAttribute("data-server-rendered", "true");
+};
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <HelmetProvider>
+//       <Router />
+//     </HelmetProvider>
+//   </React.StrictMode>
+// );
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+loadComponents()
+  .then(() =>
+    hydrateRoot(
+      rootElement,
+      <HelmetProvider>
+        <Router />
+      </HelmetProvider>
+    )
+  )
+  .catch(() =>
+    root.render(
+      <HelmetProvider>
+        <Router />
+      </HelmetProvider>
+    )
+  );
+// if (rootElement.hasChildNodes()) {
+//   hydrateRoot(
+//     rootElement,
+//       <HelmetProvider>
+//         <Router />
+//       </HelmetProvider>
+//   );
+// } else {
+//   root.render(
+//     <React.StrictMode>
+//       <HelmetProvider>
+//         <Router />
+//       </HelmetProvider>
+//     </React.StrictMode>
+//   );
+// }
 reportWebVitals();
